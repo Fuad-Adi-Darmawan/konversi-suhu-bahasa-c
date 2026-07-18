b718755eca0da013f4ff8a0d9dd7b001e5508ee4
FAD_ADS <fuadadi137@gmail.com>
2026-07-19 00:04:34 +0700

    Add files via upload

---
+++ main2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Program Konversi Suhu \n");
+
+    float celc, fahr, kelv, ream ;
+
+    printf("Input suhu celsius: ");
+    scanf("%f",&celc);
+    printf("\n");
+
+    fahr = (9.0/5.0 * celc) + 32;
+    kelv = celc + 273.15;
+    ream = celc + (4.0/5.0) ;
+
+    printf("%.2f derajat Celcius = %.2f derajat Fahrenheit \n",celc,fahr);
+    printf("%.2f derajat Celcius = %.2f derajat Kelvin \n",celc,kelv);
+    printf("%.2f derajat Celcius = %.2f derajat Reamur \n",celc,ream);
+
+    return 0;
+}
